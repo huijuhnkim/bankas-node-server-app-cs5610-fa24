a6a73cdc249d159c5bcb0bf7f042473558f531e3
huijuhnkim <huijuhnkim.dev@gmail.com>
2024-11-19 15:35:52 -0500

    Finished Lab 5

--- App.js
+++ App.js
@@ -3,10 +3,16 @@
 import express from 'express';
 import HelloRoute from './Hello.js';
 import Lab5 from "./Lab5/index.js";
+import cors from "cors";
 
 const app = express();
+app.use(cors());
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+})
 
 HelloRoute(app)
 Lab5(app)
 
-app.listen("4000");
+app.listen(process.env.PORT || "4000");
