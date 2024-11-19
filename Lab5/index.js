ecafd03256de3a821df14f1cadddbf22af260ff7
huijuhnkim <huijuhnkim.dev@gmail.com>
2024-11-19 15:38:52 -0500

    Finished Lab 5 yay

--- Lab5/index.js
+++ Lab5/index.js
@@ -3,6 +3,7 @@ import QueryParameters from "./QueryParameters.js";
 import WorkingWithObjects from "./WorkingWithObjects.js";
 import WorkingWithArrays from "./WorkingWithArrays.js";
 
+// just some comment to push origin main
 export default function Lab5(app) {
     app.get("/lab5/welcome", (req, res) => {
         res.send("Welcome to the Lab5");
