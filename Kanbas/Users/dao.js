919d98d84d09e9fe7d7f2a719b40681b1126586f
huijuhnkim <huijuhnkim.dev@gmail.com>
2024-12-03 23:04:21 -0500

    3.6.3

--- Kanbas/Users/dao.js
+++ Kanbas/Users/dao.js
@@ -10,3 +10,12 @@ export const findUserByUsername = (username) => model.findOne({username: usernam
 export const findUserByCredentials = (username, password) => model.findOne({username, password})
 export const updateUser = (userId, user) => model.updateOne({id: userId, $set: user})
 export const deleteUser = (userId) => model.deleteOne({id: userId})
+
+export const findUsersByRole = (role) => model.find({role: role})
+
+export const findUsersByPartialName = (partialName) => {
+    const regex = new RegExp(partialName, "i");
+    return model.find({
+        $or: [{ firstName: { $regex: regex } }, { lastName: { $regex: regex } }],
+    })
+}
