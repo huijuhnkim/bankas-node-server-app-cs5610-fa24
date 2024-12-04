153fdd1c658b3593ea781bc44187c31e236f3518
huijuhnkim <huijuhnkim.dev@gmail.com>
2024-12-03 22:16:47 -0500

    3.6.2

--- Kanbas/Users/routes.js
+++ Kanbas/Users/routes.js
@@ -5,34 +5,37 @@ import * as enrollmentsDao from "../Enrollments/dao.js";
 export default function UserRoutes(app) {
     const createUser = (req, res) => { };
     const deleteUser = (req, res) => { };
-    const findAllUsers = (req, res) => { };
+    const findAllUsers = async (req, res) => {
+        const users = await dao.findAllUsers();
+        res.json(users);
+    };
     const findUserById = (req, res) => { };
 
-    const updateUser = (req, res) => {
+    const updateUser = async (req, res) => {
         const userId = req.params.userId;
         const userUpdates = req.body;
         dao.updateUser(userId, userUpdates);
-        const currentUser = dao.findUserById(userId);
+        const currentUser = await dao.findUserById(userId);
         req.session["currentUser"] = currentUser;
         res.json(currentUser);
     };
 
-    const signup = (req, res) => {
-        const user = dao.findUserByUsername(req.body.username);
+    const signup = async (req, res) => {
+        const user = await dao.findUserByUsername(req.body.username);
         if (user) {
             res.status(400).json(
                 { message: "Username already in use" });
             return;
         }
-        const currentUser = dao.createUser(req.body);
+        const currentUser = await dao.createUser(req.body);
         req.session["currentUser"] = currentUser;
         res.json(currentUser);
         res.json(currentUser);
     };
 
-    const signin = (req, res) => {
+    const signin = async (req, res) => {
         const { username, password } = req.body;
-        const currentUser = dao.findUserByCredentials(username, password);
+        const currentUser = await dao.findUserByCredentials(username, password);
         if (currentUser) {
             req.session["currentUser"] = currentUser;
             res.json(currentUser);
@@ -44,8 +47,8 @@ export default function UserRoutes(app) {
         req.session.destroy();
         res.sendStatus(200);
     };
-    const profile = (req, res) => {
-        const currentUser = req.session["currentUser"];
+    const profile = async (req, res) => {
+        const currentUser = await req.session["currentUser"];
         if (!currentUser) {
             res.sendStatus(401);
             return
